6e8f88be45865503f9350ce63c566453e1c5c348
Isabela Neri <isabelandss@gmail.com>
2021-04-21 15:20:03 -0300

    message service

--- src/services/Message.ts
+++ src/services/Message.ts
@@ -1,5 +1,22 @@
+import { getCustomRepository } from "typeorm"
+import { MessagesRepository } from "../repositories"
+
+interface Message {
+  admin_id: string
+  text: string
+  user_id: string
+}
+
 const MessagesService = {
-  create: async  () => {}
+  create: async  ({ admin_id, text, user_id }: Message) => {
+    const messageRepository = getCustomRepository(MessagesRepository)
+
+    const message = messageRepository.create({ admin_id, text, user_id })
+
+    const response = await messageRepository.save(message)
+
+    return response
+  }
 }
 
 export default MessagesService

012d4f09a12b93272f82e48f8a4d7a39e759aa1f
Isabela Neri <isabelandss@gmail.com>
2021-04-23 11:32:19 -0300

    client

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,17 +1,26 @@
 import { io } from '../http'
-import { ConnectionsService, UsersService } from '../services'
+import { ConnectionsService, UsersService, MessagesService } from '../services'
+
+interface Params {
+  text: string
+  email: string
+}
 
 io.on('connect', socket => {
   socket.on('client_first_access', async params => {
     const socket_id = socket.id
-    const { text, email } = params
+    const { text, email } = params as Params
+    let user_id = null
 
     const userExists = await UsersService.findByEmail({ email })
 
     if(!userExists) {
       const user = await UsersService.create({ email })
+      user_id = user.id
       return await ConnectionsService.create({ socket_id, user_id: user.id })
     }
+
+    user_id = userExists.id
 
     const connection = await ConnectionsService.findByUserId({ user_id: userExists.id })
 
@@ -21,5 +30,7 @@ io.on('connect', socket => {
       connection.socket_id = socket_id
       await ConnectionsService.create(connection)
     }
+
+    await MessagesService.create({ text, user_id })
   })
 })

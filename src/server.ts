72adaa6b217e54498a253fb170b7e5d824babec7
Isabela Neri <isabelandss@gmail.com>
2021-04-23 09:51:58 -0300

    public

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import express from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+import path from 'path'
 import './database'
 import { routes } from './routes'
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const http = createServer(app)
+const io = new Server(http)
+
+io.on('connection', (socket: Socket) => console.log('se conectou', socket.id))
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.set('views', path.join(__dirname, '..', 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+app.get('/', (req, res) => res.render('html/client.html'))
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => console.log(`server is running on port ${PORT}`))
